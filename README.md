81d41cf7e1b4788a80f0fa10db2cdeb8f055c442
dyoung <daniel.young@sigma-systems.com>
2017-02-13 21:49:05 +0000

    Added configuration option for the max-params rule. Also upgrade TSLint dependency to 4.4.0

--- README.md
+++ README.md
@@ -4,10 +4,6 @@ A collection of additional rules that can be used by an existing TSLint installa
 
 ## Rules
 
-| Name | Options | Description | Rationale |
-| ---- | ---- | ---- | ---- |
-| `max-params` | None | Checks function and method declarations and expressions to see if more than 3 parameters are used, and flags them accordingly | Having a large number of parameters is a warning sign of a method that is doing too much |
-
-### Coming soon
-
-Add configuration option to set the maximum allowed parameters in `max-params` rule.
+| Name         | Options                                                             | Description                                                                                                                                          | Rationale |
+| ------------ | ------------------------------------------------------------------- | ---------------------------------------------------------------------------------------------------------------------------------------------------- | --------- |
+| `max-params` | Can specify a maximum number of allowed parameters e.g. `[true, 5]` | Checks function and method declarations and expressions to see if more than the allowed number of parameters are defined, and flags them accordingly | Having a large number of parameters is a warning sign of a method that is doing too much |
--- src/maxParamsRule.ts
+++ src/maxParamsRule.ts
@@ -12,93 +12,263 @@ import * as Lint from "tslint";
  */
 export class Rule extends Lint.Rules.AbstractRule
 {
+    /**
+     * The default value to use when no configuration is provided
+     * 
+     * @static
+     * @type {number}
+     * @memberOf Rule
+     */
+    public static readonly DEFAULT_MAX_NUMBER: number = 3;
+
+    /**
+     * The minimum value that could possibly be used
+     * 
+     * @static
+     * @type {number}
+     * @memberOf Rule
+     */
+    public static readonly MINIMUM_MAX_NUMBER: number = 0;
+
     public static metadata: Lint.IRuleMetadata = {
         ruleName: "max-params",
         type: "maintainability",
         description: "Checks the number of parameters in a function or method declaration",
-        options: null/*{
+        options: {
             type: "number",
             minimum: 0
-        }*/,
-        optionsDescription: ""/*"You can configure the maximum number of allowable parameters. If not configured, the default value of '3' is used."*/,
+        },
+        optionsDescription: "An optional maximum number of parameters allowed. If not provided a default value of 3 is used.",
+        optionExamples: ["true", "[true, 4]"],
         typescriptOnly: false,
-        rationale: "Too many parameters is a sign of code that does too much. More than 3 is discouraged"
+        rationale: "Too many parameters is a sign of code that does too much."
+    };
+
+    /**
+     * Retrieves the failure string based on the parameters provided and the configured maximum
+     * 
+     * @static
+     * @param {number} expected The maximum allowable parameters
+     * @param {number} actual The actual number of provided parameters
+     * @returns {string} The failure string to return to the linter
+     * 
+     * @memberOf Rule
+     */
+    public static FAILURE_STRING(expected: number, actual: number): string
+    {
+        return `Number of parameters (${actual}) exceeds the maximum allowed of ${expected}.`;
     };
 
     /**
      * Applies the MaxParamsWalker to the supplied source file
      * 
      * @param {ts.SourceFile} sourceFile The file that will be walked for rule violations
-     * 
      * @returns {Array<Lint.RuleFailure>} An array of failures caused by too many parameters
      * 
      * @memberOf Rule
      */
     public apply(sourceFile: ts.SourceFile): Array<Lint.RuleFailure>
     {
-        return this.applyWithWalker(new MaxParamsWalker(sourceFile, this.getOptions()));
+        return this.applyWithWalker(new MaxParamsWalker(sourceFile, this.getOptions(), this.maximum));
+    }
+
+    /**
+     * Getter for the maximum number of parameters allowed.
+     * 
+     * Looks for a configured option, and returns the default if none is found
+     * 
+     * @readonly
+     * @type {number}
+     * @memberOf Rule
+     */
+    public get maximum(): number
+    {
+        const ruleArgs = this.getOptions().ruleArguments;
+
+        if (ruleArgs[0] !== undefined)
+        {
+            return ruleArgs[0];
+        }
+
+        return Rule.DEFAULT_MAX_NUMBER;
+    }
+
+    /**
+     * Indicates whether or not the rule should be enabled.
+     * 
+     * Will return false if the provided maximum value is not a number or is a negative number
+     * 
+     * @returns {boolean}
+     * 
+     * @memberOf Rule
+     */
+    public isEnabled(): boolean
+    {
+        const configurationIsValid: boolean = typeof this.maximum === "number" && this.maximum >= Rule.MINIMUM_MAX_NUMBER;
+
+        return super.isEnabled() && configurationIsValid;
     }
 }
 
+/**
+ * Walker that examines statements that contain parameters, and ensures that they do not exceed the maximum.
+ * 
+ * @class MaxParamsWalker
+ * @extends {Lint.RuleWalker}
+ */
 class MaxParamsWalker extends Lint.RuleWalker
 {
-    public static readonly DEFAULT_MAX_NUMBER: number = 3;
+    /**
+     * Creates an instance of MaxParamsWalker.
+     * 
+     * @param {ts.SourceFile} sourceFile The sourcefile to walk
+     * @param {Lint.IOptions} options Any configured options
+     * @param {number} maximum The maximum amount of parameters that should be allowed
+     * 
+     * @memberOf MaxParamsWalker
+     */
+    constructor(sourceFile: ts.SourceFile, options: Lint.IOptions, private maximum: number)
+    {
+        super(sourceFile, options);
+    }
 
+    /**
+     * Examine a function declaration node
+     * 
+     * @param {ts.FunctionDeclaration} node The node that may contain parameters to check
+     * 
+     * @memberOf MaxParamsWalker
+     */
     public visitFunctionDeclaration(node: ts.FunctionDeclaration)
     {
-        this.checkParameters(node);
+        if (this.parametersExceedMaximum(node))
+        {
+            this.applyFailure(node);
+        };
+
         super.visitFunctionDeclaration(node);
     }
 
+    /**
+     * Examine a function expression node
+     * 
+     * @param {ts.FunctionExpression} node The node that may contain parameters to check
+     * 
+     * @memberOf MaxParamsWalker
+     */
     public visitFunctionExpression(node: ts.FunctionExpression)
     {
-        this.checkParameters(node);
+        if (this.parametersExceedMaximum(node))
+        {
+            this.applyFailure(node);
+        };
+
         super.visitFunctionExpression(node);
     }
 
+    /**
+     * Examine a constructor node
+     * 
+     * @param {ts.ConstructorDeclaration} node The node that may contain parameters to check
+     * 
+     * @memberOf MaxParamsWalker
+     */
     public visitConstructorDeclaration(node: ts.ConstructorDeclaration): void
     {
-        this.checkParameters(node);
+        if (this.parametersExceedMaximum(node))
+        {
+            this.applyFailure(node);
+        };
+        
         super.visitConstructorDeclaration(node);
     }
 
+    /**
+     * Examine a method declaration
+     * 
+     * @param {ts.MethodDeclaration} node The node that may contain parameters to check
+     * 
+     * @memberOf MaxParamsWalker
+     */
     public visitMethodDeclaration(node: ts.MethodDeclaration): void
     {
-        this.checkParameters(node);
+        if (this.parametersExceedMaximum(node))
+        {
+            this.applyFailure(node);
+        };
+        
         super.visitMethodDeclaration(node);
     }
 
+    /**
+     * Examine a method signature
+     * 
+     * @param {ts.SignatureDeclaration} node The node that may contain parameters to check
+     * 
+     * @memberOf MaxParamsWalker
+     */
     public visitMethodSignature(node: ts.SignatureDeclaration): void
     {
-        this.checkParameters(node);
+        if (this.parametersExceedMaximum(node))
+        {
+            this.applyFailure(node);
+        };
+        
         super.visitMethodSignature(node);
     }
 
-    public visitArrowFunction(node: ts.FunctionLikeDeclaration): void
+    /**
+     * Examine an arrow function
+     * 
+     * @param {ts.ArrowFunction} node The node that may contain parameters to check
+     * 
+     * @memberOf MaxParamsWalker
+     */
+    public visitArrowFunction(node: ts.ArrowFunction): void
     {
-        this.checkParameters(node);
+        if (this.parametersExceedMaximum(node))
+        {
+            this.applyFailure(node);
+        };
+        
         super.visitArrowFunction(node);
     }
 
-    private checkParameters(node: ts.SignatureDeclaration): void
+    /**
+     * Checks the parameters contained in the supplied node against 
+     * 
+     * @private
+     * @param {ts.SignatureDeclaration} node The node to check
+     * @returns {boolean} Whether or not the maximum has been exceeded
+     * 
+     * @memberOf MaxParamsWalker
+     */
+    private parametersExceedMaximum(node: ts.SignatureDeclaration): boolean
     {
-        if (node.parameters.length > MaxParamsWalker.DEFAULT_MAX_NUMBER)
-        {
-            this.applyFailure(node);
-        }
+        return node.parameters.length > this.maximum
     }
 
+    /**
+     * Applies a failure that will cover from the first parameter that exceeds the maximum to the last
+     * 
+     * @private
+     * @param {ts.SignatureDeclaration} node The node that caused the failure
+     * 
+     * @memberOf MaxParamsWalker
+     */
     private applyFailure(node: ts.SignatureDeclaration): void
     {
-        // Work out the beginning of the failure
-        const index = MaxParamsWalker.DEFAULT_MAX_NUMBER;
-        const paramNode = node.parameters[index];
-        const startOfFailure = paramNode.getStart();
+        const parameterLength = node.parameters.length;
 
-        // Get the width of the failure
-        const lastNode = node.parameters[node.parameters.length - 1]
-        const endOfFailure = lastNode.end;
-        const failureWidth = endOfFailure - startOfFailure;
-        this.addFailureAt(startOfFailure, failureWidth, `Number of parameters (${node.parameters.length}) exceeds the maximum allowed of ${MaxParamsWalker.DEFAULT_MAX_NUMBER}.`);
+        // Work out the beginning and width of the failure
+        const firstFailureNode: ts.ParameterDeclaration = node.parameters[this.maximum];
+        const lastFailureNode: ts.ParameterDeclaration = node.parameters[parameterLength - 1];
+       
+        const startOfFailure: number = firstFailureNode.getStart();
+        const endOfFailure: number = lastFailureNode.end;
+
+        const failureWidth: number = endOfFailure - startOfFailure;
+
+        this.addFailureAt(startOfFailure, failureWidth, Rule.FAILURE_STRING(this.maximum, parameterLength));
     }
 }
